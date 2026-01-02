3d001dc9b18d685983c5b015c328af8632c163b2
net2t <net2tara@gmail.com>
2026-01-02 05:52:37 +0500

    Selectors: store Phase 2 posts/comments selectors

--- config/selectors.py
+++ config/selectors.py
@@ -61,3 +61,50 @@ class LoginSelectors:
     PASSWORD_FIELD_1 = "#pass, input[name='pass']"
     PASSWORD_FIELD_2 = "input[type='password']"
     SUBMIT_BUTTON = "button[type='submit'], form button"
+
+
+class PostSelectors:
+    """Selectors for posts pages (stored for Phase 2 use; not used in Phase 1)."""
+
+    POST_CONTAINER = "article.bas-sh"
+    AUTHOR_PROFILE_LINK = "article.bas-sh a[href^='/users/']"
+    AUTHOR_NAME = "article.bas-sh h3[itemprop='name'] bdi"
+    POST_TIME = "article.bas-sh time[itemprop='datePublished']"
+    TEXT_POST_CONTENT = "article.bas-sh h2[itemprop='text']"
+    IMAGE_POST_IMG = "article.bas-sh img[itemprop='image']"
+    REPLIES_BUTTON = "a[href*='/comments/'] button[itemprop='discussionUrl'], a[href*='/comments/'] button.vt"
+    REPLIES_COUNT = "button [itemprop='commentCount']"
+    FOLLOW_BUTTON = "form[action='/follow/add/'] button.fbtn"
+    ONE_ON_ONE_BUTTON = "form[action^='/1-on-1/'] button"
+    SHARE_BUTTON_IMAGE = "form[action='/1-on-1/share/photo/'] button"
+    LINK_BUTTON = "a[href^='/content/'] button.link"
+    REPORT_BUTTON = "form[action='/report-or-block/'] button.report"
+    CATEGORY_SUGGEST_BUTTON = "form[action='/category/suggest/'] button.categbtns"
+
+
+class CommentSelectors:
+    """Selectors for comments pages (stored for Phase 2 use; not used in Phase 1)."""
+
+    COMMENTS_PAGE_ROOT = "div.bas-sh"
+    BACK_TO_POSTS_BUTTON = "form[action='/redirect-to-content/'] button"
+    MAIN_POST_WRAPPER = "section[itemtype*='SocialMediaPosting']"
+    POST_AUTHOR_LINK = "section a[href^='/users/'] h3 bdi"
+    POST_PUBLISHED_TIME = "section time[itemprop='datePublished']"
+    POST_TEXT_CONTENT = "h2[itemprop='text']"
+    MAIN_IMAGE = "img[itemprop='image']"
+    REPLY_ANCHOR = "a[name='reply']"
+
+    REPLY_FORM = "form[action='/direct-response/send/']"
+    REPLY_CSRF_TOKEN = "form[action='/direct-response/send/'] input[name='csrfmiddlewaretoken']"
+    REPLY_TEXTAREA = "textarea#id_direct_response, textarea[name='direct_response']"
+    SEND_REPLY_BUTTON = "form[action='/direct-response/send/'] button[type='submit']"
+    REFRESH_BUTTON = "form[action^='/comments/'] button.rf"
+
+    FOLLOW_TO_REPLY_MARKER = "mark"
+
+    COMMENT_ROW = "div[style*='overflow:hidden']"
+    COMMENT_AUTHOR = "a[href^='/users/'] bdi b"
+    COMMENT_TEXT = "span.nos.lsp.bw bdi"
+    COMMENT_TIME = "span.cxs.cgy bdi"
+    BLOCK_BUTTON = "form[action='/block/user/'] button"
+    INLINE_REPLY_BUTTON = "form[action='/direct-response/create/'] button.dir_rep"
